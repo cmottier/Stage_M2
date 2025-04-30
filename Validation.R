e0eee842de0ebb552b7bca3dfb578335c154cb3b
Camille MOTTIER <mottier@newcefe.newage.fr>
2025-04-30 13:22:35 +0200

    validation

--- Validation.R
+++ Validation.R
@@ -13,6 +13,8 @@ library(spatstat)
 
 load("RData/5km2/grid_sf_5km2.RData")
 
+load("Resultats_MCMC/5km2_sans_lasso/IEP_2021.RData")
+
 periode = (2010:2024)
 
 dpts_occitanie <- st_read("Data/departements-d-occitanie.shp") 
@@ -29,6 +31,7 @@ nutria_periode <- st_read("Data/CEN_2025/Ragondin_rat_musque_pts_2025.shp") %>%
 
 
 # Essai quadrat method (pas adapté à notre modèle) -----------------------------
+
 # https://www.paulamoraga.com/book-spatial/complete-spatial-randomness.html
 
 ## à la main #################
@@ -53,7 +56,7 @@ for (annee in 2010:2024) {
 ## avec le package spatstat #######################
 
 
-annee <- 2013
+annee <- 2021
 
 nutria <- nutria_periode %>%
   filter(year == annee)
@@ -69,3 +72,34 @@ Q <- quadratcount(X, nx = 100, ny = 50)
 quadrat.test(Q) # pas significatif : pas CSR
 quadrat.test(Q, "regular") # ce test est significatif : clustered pas rejeté
 quadrat.test(Q, "clustered")
+
+
+# K fonction de Ripley ---------------------------------------------------------
+
+# test complete spatial randomness (PPP homogène)
+envelope_K_csr <- envelope(X, Kest, nsim = 99)
+plot(envelope_K_csr, main = "CSR Test with Envelopes")
+# évidemment pas adapté !
+
+
+# estimate intensity at each point location
+intensite <- function(x,y) {
+  pt <- st_point(c(x,y))
+  cell <- which(lengths(st_intersects(IEP_2021, pt))>0)
+  return(IEP_2021$lambda_med[cell])
+}
+
+lambda_hat <- function(x_vec,y_vec) { 
+  # doit marcher en vectoriel
+  apply(matrix(c(x_vec, y_vec), ncol = 2, byrow = FALSE), 1, intensite)
+}
+
+env_inhom <- envelope(pinhom, Kinhom, nsim = 99,
+                      simulate = expression(rpoispp(lambda_hat, win = as.owin(occitanie))))
+plot(env_inhom, main = "Inhomogeneous K-function with Envelopes")
+
+# Now we're asking: 
+# "Given the inhomogeneous intensity, are the points still more/less clustered than expected?"
+
+# if from a fitted model, just replace lambda_hat in rpoispp(lambda_hat)
+# by the estimated lambda
